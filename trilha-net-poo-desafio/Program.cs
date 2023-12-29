5a1b179fb05486ab4ac90776dc25cb91b9485a38
alexklenio <alexlopes.see@gmail.com>
2023-12-28 23:38:32 -0300

    organização dos desafios

---
+++ trilha-net-poo-desafio/Program.cs
@@ -0,0 +1,19 @@
+﻿using DesafioPOO.Models;
+
+// TODO: Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine("Smartphone Nokia: ");
+Smartphone nokia = new Nokia(numero: "819123345678", modelo: "C12", imei: "asdfgh123", memoria: 64);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone iPhone: ");
+Smartphone iphone = new Iphone(numero: "819123345678", modelo: "15 PRO MAX", imei: "asdfgh123", memoria: 1024);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Telegram");
+
+
